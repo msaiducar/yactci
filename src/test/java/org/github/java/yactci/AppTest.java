f6708aabd531939dd1b78c8713a17e7ef30b4247
Said Ucar <msaiducar@gmail.com>
2020-01-30 21:39:49 +0300

    project init. maven-junit5

---
+++ src/test/java/org/github/java/yactci/AppTest.java
@@ -0,0 +1,13 @@
+package org.github.java.yactci;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
