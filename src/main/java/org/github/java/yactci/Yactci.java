bb3f611c4627af3b2512f3aae2084b8ca9467a07
Said Ucar <msaiducar@gmail.com>
2020-02-01 15:45:30 +0300

    pipeline test ccommit

--- src/main/java/org/github/java/yactci/Yactci.java
+++ src/main/java/org/github/java/yactci/Yactci.java
@@ -5,7 +5,6 @@ public class Yactci {
   public static void main(String[] args) {
     System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
     System.out.println("yet another cracking the coding interview...");
-    System.out.println("                                        2020");
     System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
   }
 }
