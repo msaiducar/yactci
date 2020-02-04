690fc7d078514a2c14de73d3df3201c5b79fdbb7
Said Ucar <msaiducar@gmail.com>
2020-02-04 19:44:34 +0300

    first chapter second question ready to solve

---
+++ solution-api/src/main/java/org/github/java/yactci/solutionapi/C1Q2_CheckPermutationAlgorithm.java
@@ -0,0 +1,53 @@
+package org.github.java.yactci.solutionapi;
+
+import org.github.java.yactci.coreapi.annotation.BigO;
+import org.github.java.yactci.coreapi.annotation.SpaceComplexity;
+import org.github.java.yactci.coreapi.annotation.TimeComplexity;
+
+import java.util.Random;
+
+/**
+ * Chapter I - Question II
+ *
+ * <p>Given two strings, write a method to decide if one is a permutation of the other.
+ */
+public interface C1Q2_CheckPermutationAlgorithm {
+
+  /**
+   * given strings, checks if one is a permutation of the other
+   *
+   * @param first first string
+   * @param second second string
+   * @return true if one is a permutation of the other
+   */
+  boolean stringsArePermutation(String first, String second);
+
+  /**
+   * Fisher-Yates Shuffle Algorithm
+   *
+   * <p>simply shuffles the chars in the given string in a linear time and space complexity.
+   *
+   * <p>https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+   *
+   * @param str strings to be shuffled
+   * @return shuffled string
+   */
+  @TimeComplexity(best = BigO.BIG_O_N, average = BigO.BIG_O_N, worst = BigO.BIG_O_N)
+  @SpaceComplexity(best = BigO.BIG_O_N, average = BigO.BIG_O_N, worst = BigO.BIG_O_N)
+  default String shuffleString(String str) {
+    Random r = new Random();
+    char[] arr = str.toCharArray();
+
+    for (int i = arr.length - 1; i > 0; i--) {
+      // Pick a random index from 0 to i
+      int j = r.nextInt(i + 1);
+
+      // Swap arr[i] with the element at random index
+      char temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+
+    return new String(arr);
+  }
+}
---
+++ solution-api/src/main/java/org/github/java/yactci/solutionapi/C1Q2_CheckPermutationAlgorithmTest.java
@@ -0,0 +1,55 @@
+package org.github.java.yactci.solutionapi;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Chapter I - Question II
+ *
+ * <p>Given two strings, write a method to decide if one is a permutation of the other.
+ */
+public interface C1Q2_CheckPermutationAlgorithmTest<E extends C1Q2_CheckPermutationAlgorithm> {
+
+  /**
+   * To take leverage of existing tests for the algorithm, implement this method to return your
+   * implementation of {@link C1Q2_CheckPermutationAlgorithm}
+   *
+   * @return implementation of {@link C1Q2_CheckPermutationAlgorithm}
+   */
+  E createAlgorithm();
+
+  @Test
+  default void nullInputTest() {
+    assertThrows(
+        NullPointerException.class, () -> createAlgorithm().stringsArePermutation(null, ""));
+    assertThrows(
+        NullPointerException.class, () -> createAlgorithm().stringsArePermutation("", null));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"", "42", "qwerty", "long long ago in a galaxy far far away"})
+  default void sameInputTest(String str) {
+    assertThat(createAlgorithm().stringsArePermutation(str, str)).isTrue();
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"qwerty", "allons-y alonso", "long long ago in a galaxy far far away"})
+  default void impossibleBySizeTest(String str) {
+    assertThat(createAlgorithm().stringsArePermutation(str, str.concat("42"))).isFalse();
+    assertThat(createAlgorithm().stringsArePermutation(str.concat("42"), str)).isFalse();
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"qwerty", "allons-y alonso", "long long ago in a galaxy far far away"})
+  default void shuffledStringTest(String str) {
+    E permutationAlgorithm = createAlgorithm();
+    assertThat(
+            permutationAlgorithm.stringsArePermutation(
+                str, permutationAlgorithm.shuffleString(str)))
+        .isTrue();
+  }
+}
